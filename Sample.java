7aaa978f4cdbf139b4e7c160486d511bcaf2a5cc
simituli <anupama.tuli@gmail.com>
2024-10-10 22:26:49 -0700

    Update Sample.java
    Remove nth node from the end

--- Sample.java
+++ Sample.java
@@ -64,4 +64,47 @@ class Solution {
         return prev;
     }
 }
+
+// Remove Nth node from the end
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head==null) return head;
+        
+        //ListNode dummy = new ListNode(-1);
+        //dummy.next = head;
+        ListNode slow = head;
+        ListNode fast = head;
+        for(int i = 0; i<n;i++)
+        {   
+            fast=fast.next;  
+        }
+        if(fast==null)
+        {
+            head = head.next;
+            return head;
+        }
+        
+        while(fast.next!=null)
+        {
+            fast = fast.next;
+            slow=slow.next;
+        }
+         
+        slow.next = slow.next.next;
+        
+        return head;
+    }
+}
 // Your code here along with comments explaining your approach
